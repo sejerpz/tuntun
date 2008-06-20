ae3a240177d6db5348f9ddf08a1be8680915983b
sejerpz <sejerpz@68fa7cd3-4548-0410-b5be-81118fefee20>
2008-06-20 12:03:48 +0000

    merged gnio branch changed r99:103 into trunk

---
+++ trunk/gnio/gnio/gnio.h
@@ -0,0 +1,44 @@
+/* GNIO - GLib Network Layer of GIO
+ *
+ * Copyright (C) 2008 Christian Kellner, Samuel Cormier-Iijima
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Authors: Christian Kellner <gicmo@gnome.org>
+ *          Samuel Cormier-Iijima <sciyoshi@gmail.com>
+ */
+
+#ifndef __GNIO_H__
+#define __GNIO_H__
+
+#define __GNIO_GNIO_H_INSIDE__
+
+#include <gnio/ginetaddress.h>
+#include <gnio/ginet4address.h>
+#include <gnio/ginet6address.h>
+#include <gnio/ginetsocketaddress.h>
+#include <gnio/gsocketaddress.h>
+#include <gnio/gresolver.h>
+#include <gnio/gsocket.h>
+#include <gnio/gnioerror.h>
+#include <gnio/gsocketconnection.h>
+#include <gnio/gsocketserver.h>
+#include <gnio/gtcpconnection.h>
+#include <gnio/gtcpserver.h>
+
+#undef __GNIO_GNIO_H_INSIDE__
+
+#endif /* __GNIO_H__ */
